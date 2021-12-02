df691e52b71398c9b58a292491b4e034fb08dc21
Sagar Jaid <sagarjaid321@gmail.com>
2021-12-02 22:48:58 +0530

    troislogic website v4.5

---
+++ cost.js
@@ -0,0 +1,172 @@
+let total = 0;
+
+let pwNewTotal = function pwClicked() {
+  let checkBox = document.getElementById('pw');
+
+  if (checkBox.checked == true) {
+    console.log('Checkbox is checked..');
+    total = total + 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  } else {
+    console.log('Checkbox is not checked..');
+    total = total - 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  }
+};
+
+let msNewTotal = function msClicked() {
+  let checkBox = document.getElementById('ms');
+
+  if (checkBox.checked == true) {
+    console.log('Checkbox is checked..');
+    total = total + 20000;
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    console.log(total);
+    return total;
+  } else {
+    console.log('Checkbox is not checked..');
+    total = total - 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  }
+};
+
+let ecNewTotal = function ecClicked() {
+  let checkBox = document.getElementById('ec');
+
+  if (checkBox.checked == true) {
+    console.log('Checkbox is checked..');
+    total = total + 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  } else {
+    console.log('Checkbox is not checked..');
+    total = total - 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  }
+};
+
+let appNewTotal = function appClicked() {
+  let checkBox = document.getElementById('app');
+
+  if (checkBox.checked == true) {
+    console.log('Checkbox is checked..');
+    total = total + 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  } else {
+    console.log('Checkbox is not checked..');
+    total = total - 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  }
+};
+
+let iosAppNewTotal = function iosAppClicked() {
+  let checkBox = document.getElementById('iosapp');
+
+  if (checkBox.checked == true) {
+    console.log('Checkbox is checked..');
+    total = total + 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  } else {
+    console.log('Checkbox is not checked..');
+    total = total - 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  }
+};
+
+let bscNewTotal = function bscClicked() {
+  let checkBox = document.getElementById('bsc');
+
+  if (checkBox.checked == true) {
+    console.log('Checkbox is checked..');
+    total = total + 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  } else {
+    console.log('Checkbox is not checked..');
+    total = total - 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  }
+};
+
+let ddsNewTotal = function ddsClicked() {
+  let checkBox = document.getElementById('dds');
+
+  if (checkBox.checked == true) {
+    console.log('Checkbox is checked..');
+    total = total + 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  } else {
+    console.log('Checkbox is not checked..');
+    total = total - 20000;
+    console.log(total);
+
+    var calc = document.getElementById('caltotal');
+    calc.innerText = total;
+
+    return total;
+  }
+};
